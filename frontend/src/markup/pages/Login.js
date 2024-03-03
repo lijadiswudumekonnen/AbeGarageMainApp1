a410cd8608b076cb2b145a0a709cba696e45fabb
lijmekonnen <lijadismekonnen2019@gmail.com>
2024-03-02 21:46:12 -0500

    commit

---
+++ frontend/src/markup/pages/Login.js
@@ -0,0 +1,13 @@
+import React from "react";
+import LoginForm from "../components/LoginForm/LoginForm";
+// Import the Login component
+
+function Login(props) {
+  return (
+    <div>
+       <LoginForm/>
+    </div>
+  );
+}
+
+export default Login;
